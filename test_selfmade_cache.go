07e2e2b048b68f085072ec75feba42750bfc076a
Makmanu <vfrfv0640@gmail.com>
2025-05-16 16:28:01 +0300

    add 2 new same tests with cache

--- test_selfmade_cache.go
+++ test_selfmade_cache.go
@@ -5,17 +5,29 @@ import(
 	"time"
 )
 
-func fib(n int) int{
-    if n < 2{
-        return n
+func fib_cache() func(int) int{
+	cache := map[int]int{}
+
+	var fib func(n int) int
+	fib = func(n int) int{
+		if n < 2{
+			return n
+		}
+		if val, ok := cache[n]; ok {
+			return val
+		}
+		result := fib(n-1) + fib(n-2)
+		cache[n] = result
+		return result
 	}
-    return fib(n-1) + fib(n-2)
+	return fib
 }
+
 
 func main(){
 	timeStart := time.Now()
     for i := 0; i < 45; i++ {
-        fmt.Println(fib(i))
+        fmt.Println(fib_cache()(i))
 		fmt.Println("		", time.Since(timeStart))
 		if time.Since(timeStart).Seconds() > 10{
 			return
